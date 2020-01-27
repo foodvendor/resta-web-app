11884b091796dd1b6066763af859aa71fd494146
Zagade Kaustubh <pranjan.bhure14@vit.edu>
2020-01-28 00:02:47 +0530

    Custom exceptions

---
+++ restful-web-services/src/main/java/com/NVDabbewala/rest/webservices/restfulwebservices/customexception/BillException.java
@@ -0,0 +1,9 @@
+package com.NVDabbewala.rest.webservices.restfulwebservices.customexception;
+
+public class BillException extends Exception {
+
+	public BillException(String arg0) {
+		super(arg0);
+	}
+
+}
