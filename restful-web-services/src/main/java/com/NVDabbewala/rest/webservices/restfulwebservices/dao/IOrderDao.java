12a6e8f6e4f4ba4517de8b6e2e1c0468dc234435
Zagade Kaustubh <pranjan.bhure14@vit.edu>
2020-01-30 09:59:17 +0530

    Minor bug fixes

---
+++ restful-web-services/src/main/java/com/NVDabbewala/rest/webservices/restfulwebservices/dao/IOrderDao.java
@@ -0,0 +1,18 @@
+package com.NVDabbewala.rest.webservices.restfulwebservices.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.NVDabbewala.rest.webservices.restfulwebservices.pojos.Order;
+
+@Repository
+public interface IOrderDao extends JpaRepository<Order, Integer> {
+
+	@Query("FROM Order o INNER JOIN o.orderStudentId s INNER JOIN o.orderMenuId m  where s.studentId=:studId")
+//	@Query("SELECT o.orderId FROM Order o INNER JOIN o.orderStudentId s where s.studentId=:studId")
+	List<Order> getAllOrdersByStudentId(@Param("studId") int studentId);
+}
